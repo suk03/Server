a1d4bdc4cc7384b3c96d2a6ac814491555b63267
Sukrithi <sonusukrithi@gmail.com>
2024-10-16 11:36:58 +0530

    updated server

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 5000;// GitHub Personal Access Token
 const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
 // CORS configuration
 const corsOptions = {
-  origin: ['https://github-ui-mu.vercel.app', 'http://localhost:3000'],
+  origin: ['https://github-issue-eight.vercel.app', 'http://localhost:3000'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
