56019c109ba5b64c3d356184ccb4a7b0ab40b307
Sukrithi <sonusukrithi@gmail.com>
2025-01-02 15:35:15 +0530

    ldje

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ const readJobsFromGithub = async () => {
         }
       }
     ); 
+    
 
 
 
@@ -336,7 +337,7 @@ app.use((err, req, res, next) => {
     error: 'Internal server error',
     details: process.env.NODE_ENV === 'development' ? err.message : undefined
   });
-});   
+});    
 
 
 
